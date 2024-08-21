db50af6c091a53de8fbab2b91bf15445137414e1
BoonyaritL <zoldyck15123@gmail.com>
2024-08-21 17:07:02 +0700

    Update route to redirect to /companies

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompanyCRUDController;
 
-Route::resource('companies',CompanyCRUDController::class);
+Route::resource('companies', CompanyCRUDController::class);
 
-Route::get('', function () {
-    return view('hello');
+Route::get('/', function () {
+    return redirect('/companies'); // เมื่อเข้าหน้าแรก จะ redirect ไปที่ /companies
 });
